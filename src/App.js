7ae531dbc588030af7eae2c05b7315a0ce6fe874
Hector Valdez <hvaldez180@gmail.com>
2019-11-11 17:41:48 -0500

    removing comments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
 import { Route } from "react-router-dom";
 
 const App = () => (
   <div>
     <Navigation />
-<Route exact path="/" componet={Home}/>
-<Route path="/about" component={About}/>
-<Route path="/contact" component={Contact}/>
-
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
 export default App;
 
-//I fixed the debug but i have a question what went wrong
-{/* <Route exact path="/" component={Home}/>
-<Route path="/about" component={About}/>
-<Route path="/contact" component={Contact}/> */}
+
